e3dca9c93198893ae53478ba405891c788c4a3ea
Val <vswehnau@gmail.com>
2026-03-06 14:51:23 -0500

    Created CountryManager class and Country struct in CountryManager class.

---
+++ src/CountryManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Val Wehnau on 06/03/2026.
+//
+
+#include "CountryManager.h"
---
+++ src/CountryManager.h
@@ -0,0 +1,26 @@
+//
+// Created by Val Wehnau on 06/03/2026.
+//
+
+#ifndef COUNTRYMANAGER_H
+#define COUNTRYMANAGER_H
+
+
+
+class CountryManager {
+
+    public:
+    struct Country
+    {
+
+    };
+    private:
+
+
+
+
+};
+
+
+
+#endif //COUNTRYMANAGER_H
