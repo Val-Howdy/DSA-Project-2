593d989532543eb358777321a91b0f43dfddbf09
Val <vswehnau@gmail.com>
2026-03-08 17:44:57 -0400

    test: created verify population import test

--- test/test.cpp
+++ test/test.cpp
@@ -1,37 +1,20 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-
-
+#include "CountyManager.h"
+#include <filesystem>
 
 using namespace std;
 
-// the syntax for defining a test is below. It is important for the name to be unique, but you can group multiple tests with [tags]. A test can have [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[flag]"){
-	// instantiate any class members that you need to test here
-	int one = 1;
 
-	// anything that evaluates to false in a REQUIRE block will result in a failing test 
-	REQUIRE(one == 0); // fix me!
+TEST_CASE("verify population import", "[Data Import]") {
+	CountyManager manager("data/Covid_data.csv", "data/Population_data.csv");
 
-	// all REQUIRE blocks must evaluate to true for the whole test to pass
-	REQUIRE(false); // also fix me!
-}
+	for (auto const& pair : manager) {
+		INFO("Checking FIPS: " << pair.first << " (" << pair.second._name << ")");
+		REQUIRE(pair.second._population > 0);
+	}
 
-TEST_CASE("Test 2", "[flag]"){
-	// you can also use "sections" to share setup code between tests, for example:
-	int one = 1;
 
-	SECTION("num is 2") {
-		int num = one + 1;
-		REQUIRE(num == 2);
-	};
-
-	SECTION("num is 3") {
-		int num = one + 2;
-		REQUIRE(num == 3);
-	};
-
-	// each section runs the setup code independently to ensure that they don't affect each other
 }
 
 
